cf9ceae56773525ea6cb6ca1025b085580dfec71
SumitDongare07 <130834889+SumitDongare07@users.noreply.github.com>
2023-05-23 15:27:27 +0530

    Add files via upload

---
+++ ActiTime.java
@@ -0,0 +1,21 @@
+package Basic;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ActiTime {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demo.actitime.com/login.do");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.id("keepLoggedInCheckBox")).click();
+		driver.findElement(By.id("loginButton")).click();
+		driver.findElement(By.id("logoutLink")).click();
+	}
+}
